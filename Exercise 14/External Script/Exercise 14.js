1f8cdd3791d7fa64b44c4f9bd5df09e36769af94
BillyDhenClirBusilan <20214200@s.ubaguio.edu>
2022-04-30 13:12:02 +0800

    Add files via Github desktop

--- Exercise 14/External Script/Exercise 14.js
+++ Exercise 14/External Script/Exercise 14.js
@@ -1,1 +1,18 @@
-document.getElementById("target").innerHTML=("Yes")
+document.getElementById("target");
+
+var Name = ("Name: Billy Dhen Clir Busilan");
+var Course = ("Course: Bachelors of Science in Computer Science");
+var Year = ("Year: First Year");
+var Section = ("Section: IAA1");
+var Subject = ("Subject: Human Computer Interaction");
+document.write(Name + "<br>" + Course + "<br>" + Year + "<br>" + Section + "<br>" + Subject);
+
+console.log (Name);
+console.log (Course);
+console.log (Year);
+console.log (Section);
+console.log (Subject);
+
+alert("Name: Billy Dhen Clir Busilan \nCourse: Bachelors of Science in Computer Science n\Year: First Year \nSection: IAA1 n\Subject: Human Computer Interaction");
+
+
